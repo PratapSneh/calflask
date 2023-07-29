365d9694a5bd0c2ea36e3f4dde8acbd287e636b0
PratapSneh <snehpratap95@gmail.com>
2023-07-29 11:28:11 +0530

    json added

--- app.py
+++ app.py
@@ -21,34 +21,37 @@ if __name__=='__main__':
 
 '''
 
-from flask import Flask,render_template
+from flask import Flask,request,render_template
+
+
 
 obj=Flask(__name__)
+
 
 @obj.route('/')
 def welcome():
     return "Welcome to Flask"
 
-@obj.rout('/cal',method=["GET"])
-def math_operation():
+@obj.route('/cal',methods=["GET"])
+def math_operator():
     operation=request.json["operation"]
     number1=request.json["number1"]
     number2=request.json["number2"]
-
-    if operation=="add":
-        result=number1+number2
-    elif operation=="multiply":
-        result=number1*number2
-    elif operation=="division":
-        result=number1/number2
-    else:
-        result=number1-number2
-    return result
     
-
+    if operation=="add":
+        result=int(number1)+int(number2)
+    elif operation=="multiply":
+        result=int(number1)*int(number2)
+    elif operation=="division":
+        result=int(number1)/int(number2)
+    else:
+        result=int(number1)-int(number2)
+    return "My operation is {} and result is {}".format(operation,result)
+        
+    
 
 
 print(__name__)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     obj.run()
